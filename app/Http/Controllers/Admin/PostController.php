5ae73c19eaec688fe02682d49b0526c816d8aad6
DevAbdelrahmanSayed <devabdelr2hman@gmail.com>
2024-05-02 01:25:55 +0300

    add the post

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::with('user')->get();
+        return view('Dashboard.post', compact('posts'));
+    }
+
+}
